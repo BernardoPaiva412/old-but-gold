9bdc2e6fa1e17b565a85a09e1c8742c7420a0b5f
Bernardo Paiva <bernardo.pava@aluno.ifsp.edu.br>
2026-06-09 08:23:04 -0300

    configurando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import AppRoutes from './Routes/AppRoutes'
+
 import './App.css'
 
 export default function App() {
   return (
     <>
-      
+      <main>
+        <AppRoutes />
+      </main>
     </>
   )
 }
