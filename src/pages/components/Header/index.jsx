e045851750b299e7567dda0f59313ccd2ed11ce1
Bernardo Paiva <bernardo.paiva@aluno.ifsp.edu.br>
2026-06-16 08:39:04 -0300

    primeira logo

---
+++ src/pages/components/Header/index.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className='header'>
+            <Link to='/' className='logo'>
+                <span className="logo-old">Old</span>
+                <span className="logo-but">But</span>
+                <span className="logo-gold">Gold</span>
+            </Link>
+        </header>
+    )
+}
